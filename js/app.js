708b092a3c00c1659b7eb6980bfac67cbfd6e70e
black script <kaantekk36+github2@gmail.com>
2023-04-29 23:02:14 +0300

    loading page added

--- js/app.js
+++ js/app.js
@@ -4,3 +4,22 @@ var typed = new Typed(".auto", {
     backSpeed: 150,
     loop: true
 });
+
+let copyLink = null;
+function linkCopy(link) {
+    copyLink = link;
+    if(copyLink == null) { copyLink = "hata oluştu :(" }
+    navigator.clipboard.writeText(copyLink);
+}
+
+$(document).ready(function() {
+    // copy success - [START]
+    $('.howToAddCode .copy').click( () => {
+        Swal.fire({
+            icon: 'success',
+            title: 'Kopyalandı',
+            text: copyLink,
+        }) 
+    });
+    // copy success - [END^]
+})
